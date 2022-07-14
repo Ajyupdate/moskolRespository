83721e96728bc7a5aaee42cf3dc2ca96235e307d
Ajibade <ajibadeemmanuel58@gmail.com>
2022-07-14 01:13:46 +0100

    footer and header fix

--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,11 @@ import facebookIcon from './image/facebookIcon.jpg';
 import TwitterIcon from './image/TwitterIcon.jpg'
 import gmailIcon from './image/gmailIcon.jpg';
 
+
 function Footer() {
   return (
-    <div className='footer'>
+    <div className='footer-wrapper'>
+        <div className='footer'>
         <div>
             <h3>
                 moskolEngineering
@@ -21,12 +23,13 @@ function Footer() {
         </div>
 
         <div>
-            <ul className='footer-links'>
-                <li>Home</li>
-                <li>CCTV</li>
-                <li>Inverter</li>
-                <li>Decoders</li>
-            </ul>
+            
+            <div className='footer-links'>
+                <p className='home'><a href='/'>HOME</a></p>
+                <p><a href='/cctv' >CCTV</a></p>
+                <p><a href='/inverters'>INVERTERS</a></p>
+                <p><a href='/batteries'>BATTERIES</a></p>
+            </div>
             <div className='social-media-icon'>
                 <div><a target="_blank" rel="noreferrer" href="https://www.facebook.com/adetomiwa.emmanuel"><img src={facebookIcon} className="img-fluid" alt='Ajibade'/></a></div>
                 <div><a target="_blank" rel="noreferrer" href="https://www.facebook.com/adetomiwa.emmanuel"><img src={TwitterIcon} className="img-fluid" alt='Ajibade'/></a></div>
@@ -35,6 +38,8 @@ function Footer() {
 
         </div>
     </div>
+    </div>
+    
   )
 }
 
